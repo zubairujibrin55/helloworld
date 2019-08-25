ba456c6b77a939847fbea3e313fbee7927e7187d
zubairujibrin55 <54513160+zubairujibrin55@users.noreply.github.com>
2019-08-25 22:11:25 +0100

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import math
+x  = input("Enter numner x: ")
+y = input("Enter number y: ")
+
+x_power_y = int(x)  **  int(y)
+print("x**y = "  + str(x_power_y))
+print("log(x) " + str(math.log(int(x),2)))
